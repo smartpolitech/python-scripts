bd3c325c00e331c44d48f6547b3d6785c7cae815
pbustos <pbustos@unex.es>
2016-04-07 00:17:28 +0200

    remove iso format from date field

--- openweathermap.py
+++ openweathermap.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Script to read temperature from openweathermap and write to a collectionn in RethinkDB
+
 import requests
 import rethinkdb as rdb
 import time, json
@@ -10,14 +12,14 @@ def readData():
 	r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Cacere&units=metric&APPID=' + conf["APPID"])
 	temp = r.json()["main"]["temp"]
 	hum = r.json()["main"]["humidity"]
-	print temp, hum, rdb.now().in_timezone('+02:00').to_iso8601()
+	#print temp, hum, rdb.now().in_timezone('+02:00')
 	conn = rdb.connect(host=conf["host"], port=conf["port"], db=conf["db"], auth_key=conf["auth_key"])
-	rdb.table(conf["table"]).insert({"date": rdb.now().in_timezone('+02:00').to_iso8601(),
-                                                       "sensors":[{"name": "temp", "value": temp},{"name": "hum", "value": hum}]}).run(conn)
+	rdb.table(conf["table"]).insert({"date": rdb.now().in_timezone('+02:00'),
+                                                       "sensors":[{"name": "temp", "value": str(temp)},{"name": "hum", "value": str(hum)}]}).run(conn)
 	conn.close()
 
 while True:
 	readData()
-	time.sleep(1800)
+	time.sleep(1000)
 
 
